f38a535f8eb70e484002c9b7b405023b75b614f7
TopGirlCoder <topgirlcoder@gmail.com>
2016-01-21 16:30:54 -0800

    update ajax-request file application.js to include AJAX

--- ajax/ajax-request/public/js/application.js
+++ ajax/ajax-request/public/js/application.js
@@ -1,9 +1,28 @@
 $(document).ready(function () {
+  var dieHtml = '<div class="die" style="display:none">'
+  dieHtml += '<span class="roll">'
+  dieHtml += -1
+  dieHtml += '</span>'
+  dieHtml += '</div>'
+  $('form').append(dieHtml);
 
-  // PSEUDO-CODE:
   //   1- intercept the form submission event using jQuery
   //   2- prevent the default action for that event from happening
   //   3- use jQuery to submit an AJAX post to the form's action
   //   4- when the AJAX post is done, display the new die roll using jQuery
+  //   5- the die shows a random number between 1 and #of sides
+  $('form').on('submit',function(e){
+    e.preventDefault();
 
+    $.ajax({
+      method: 'post',
+      url: '/rolls',
+      data: $('form').serialize(),
+    })
+
+    .done(function(data){
+      $('span.roll').text(data.value)
+      $('.die').show()
+    })
+  })
 });
